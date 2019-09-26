5ae5b14d9fb9ee77415726f2355c63e734df34b4
Maria218 <mariasitumbeko@gmail.com>
2019-09-26 16:13:44 +0200

    Added navigation transition effects

--- src/routing/Container.js
+++ src/routing/Container.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, withRouter } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import "./Fade.css"
 
 import Home from "../main/home/Home";
 import About from "../main/about/About";
 import Members from "../main/members/Members";
 import Contact from "../main/contact/Contact";
 
-export default function Container() {
+function Container({location}) {
     return (
-        <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About}/>
-            <Route path="/members" component={Members}/>
-            <Route path="/contact" component={Contact}/>
-        </Switch>
+        <TransitionGroup>
+            <CSSTransition
+                key={location.key}
+                timeout={{ enter: 300, exit: 300 }}
+                classNames={'fade'}
+            >
+                <section className="route-section">
+                    <Switch location={location}>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/about" component={About}/>
+                        <Route path="/members" component={Members}/>
+                        <Route path="/contact" component={Contact}/>
+                    </Switch>
+                </section>
+            </CSSTransition>
+        </TransitionGroup>
     )
 }
+
+export default withRouter(Container);
